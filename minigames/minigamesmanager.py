dc040a66cd903ea90fcffa8d508dd6a34fef0f6b
iamDyeus <87000693+iamDyeus@users.noreply.github.com>
2021-12-25 21:12:49 +0530

    Update minigamesmanager.py
    Removed old games and added new online browser based games.

--- minigames/minigamesmanager.py
+++ minigames/minigamesmanager.py
@@ -1,65 +1,48 @@
-#still in progress will be ready soon!
-
 import time
 
 
 def playgame():
-    print("\nNOTE : this is a BETA feature,and can make the assistant not working properly")
-    print('Which Game Would you like to play Today :')
+    print('Which Game Would you like to play Today :\n')
     print(''' 
-    list of available games:
-            \n1.Insult Battle
-            \n2.bagels
-            \n3.cannon
-            \n4.bounce
-            \n5.connect
-            \n6.flappy
-            \n7.simonsays
-            \n8.tron
+    List of Available Games:
+    \n1.Venge.io
+    \n2.Smashkarts.io
+    \n3.Skribbl.io
+    \n4.Chess
+    \n5.Drift Hunters
 
         OR type "quit" to exit gamemanager.
+        or type "help" to get help on how to use gamemanager.
 
              ''')
     
     uinp=input("Enter the number in front of the game you'll like to play\n>>")
     
-    if '1' in uinp :
-        from games import insultbattle
+    if '1' in uinp or 'Venge' in uinp or 'venge' in uinp or 'Venge.io' in uinp or 'venge.io' in uinp:
+        from games import venge
     
-
-    elif '2' in uinp:
-        from games import bagels
+    elif '2' in uinp or 'Smashkarts' in uinp or 'smashkarts' in uinp or 'Smashkarts.io' in uinp or 'smashkarts.io' in uinp:
+        from games import smashkarts
     
-    elif '3' in uinp:
-        from games import cannon
+    elif '3' in uinp or 'Skribbl' in uinp or 'skribbl' in uinp or 'Skribbl.io' in uinp or 'skribbl.io' in uinp:
+        from games import skribbl
 
-    elif '4' in uinp:
-        from games import bounce
+    elif '4' in uinp or 'Chess' in uinp or 'chess' in uinp:
+        from games import chess
 
-    elif '5' in uinp:
-        from games import connect
+    elif '5' in uinp or 'Drift' in uinp or 'drift' in uinp or 'Drift Hunters' in uinp or 'drift hunters' in uinp:
+        from games import drifthunters
 
-    elif '6' in uinp:
-        from games import flappy
-    
-    elif '7' in uinp:
-        from games import simonsays
-    
-    elif '8' in uinp:
-        from games import tron
+    elif 'help' in uinp or 'Help' in uinp or 'HELP' in uinp:
+        print("its so easy already, just type the number in front of the game you want to play")
+        print("your such an amazing person, you can't even type the number of the game you want to play")
+        print("so just type the name of the game you want to play and it will work")
 
-    elif uinp>8:
-        print('this slot is empty... Pay Dev to Add More Games XD')
-
-    elif uinp=="quit":
+    elif uinp=="quit" or uinp=="Quit":
         quit()
 
     else:
-        print('you need to choose a number from the list above NOOB! ')
-        return playgame
+        print('You need to choose a Number/Name from the list above, You NOOB! ')
+        print('\n')
+        return playgame()
     time.sleep(3)
-
-
-
-playgame()
-#after running this contine use krna,
